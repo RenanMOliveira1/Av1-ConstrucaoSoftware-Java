92002f92cfa743c8939d3101d5dc1a6344dbc8a5
tiagohs <tiago.silva.93@hotmail.com>
2015-08-09 15:54:31 -0300

    Refatoração nas Classes de Leitura, Metodos pertecentes ao ENUM feitos.

--- src/com/concessionaria/veiculos/Carro.java
+++ src/com/concessionaria/veiculos/Carro.java
@@ -19,19 +19,6 @@ public class Carro extends Veiculo {
 	public Carro() {
 	}
 	
-	/**
-	 * @param chassi
-	 * @param montadora
-	 * @param modelo
-	 * @param tipo
-	 * @param cor
-	 * @param preco
-	 * @param motolizacao
-	 * @param cambio
-	 * 
-	 * Construtor com todos os parâmentros e chamada a Super Classe Veículo
-	 */
-	
 	public Carro(String chassi, String montadora, String modelo, String tipo,
 			String cor, float preco, float motolizacao, String cambio) {
 		super(chassi, montadora, modelo, tipo, cor, preco);
@@ -39,31 +26,18 @@ public class Carro extends Veiculo {
 		setCambio(cambio);
 	}
 	
-	/**
-	 * @return Retorna a Motorização
-	 */
 	public double getMotorizacao() {
 		return motorizacao;
 	}
 	
-	/**
-	 * @param motolizacao
-	 */
 	public void setMotorizacao(double motorizacao) {
 		this.motorizacao = motorizacao;
 	}
 	
-	/**
-	 * @return Retorna o Câmbio
-	 */
 	public String getCambio() {
 		return cambio;
 	}
 	
-	/**
-	 * @param cambio
-	 * Faz a Leitura do Câmbio
-	 */
 	public void setCambio(String cambio) {
 		this.cambio = cambio;
 	}
@@ -72,41 +46,30 @@ public class Carro extends Veiculo {
 	 * Método que faz a leitura de tipo de Carro a partir do teclado
 	 */
 	public void leTipoCarro() {
-		String nomeTipoEscolhido = null;
-		
 		
 		System.out.println("\nEntre com o tipo de carro: ");
 		TipoCarro.exibirOpcoes();
 		int opcaoTipo = input.nextInt();
+		String nomeTipoEscolhido = TipoCarro.pesquisarOpcao(opcaoTipo);
 		
-		/**
-		 * Percorre todos os valores de TipoCarro e recebe seus valores em "tipoCarro" na ordem
-		 * Assim, verifica se o valor da opcao inputada confere com cada opcao de "tipoCarro"
-		 */
-		for (TipoCarro tipoCarro : TipoCarro.values()) {
-			if(opcaoTipo == tipoCarro.getNumOpcao())
-				nomeTipoEscolhido = tipoCarro.getNomeTipo();	
-		} 
+		if (nomeTipoEscolhido.equals(null))
+			throw new IllegalArgumentException("Tipo de Carro: Opção Inválida.");
 		
-		if (!nomeTipoEscolhido.equals(null))
-			setTipo(nomeTipoEscolhido);
-		else 
-			throw new IllegalArgumentException("Opcao nao valida!");
+		setTipo(nomeTipoEscolhido);	
 	}
-	
 	
 	/**
 	 * Método que faz a leitura de motorizacao de Carro a partir do teclado
 	 */
-	public void leMotorizacao() throws IllegalArgumentException {
+	public void leMotorizacao() {
 		
 		System.out.println("\nEntre com a motorizacao do tanque do carro: ");
 		int motorizacao = input.nextInt();
 		
-		if(motorizacao > 0)
-			setMotorizacao(motorizacao);
-		else
-			throw new IllegalArgumentException("Motorizacao deve ser numeral e positiva");
+		if(motorizacao < 0)
+			throw new IllegalArgumentException("Motorização: Numero Negativo não é Aceito.");
+		
+		setMotorizacao(motorizacao);	
 	}
 	
 	
@@ -114,25 +77,16 @@ public class Carro extends Veiculo {
 	 * Método que faz a leitura de tipo de Carro a partir do teclado
 	 */
 	public void leCambio() {
-		String cambioEscolhido = null;
-		
+
 		System.out.println("\nEntre com o cambio de carro: ");
 		Cambio.exibirOpcoes();
 		int opcaoCambio = input.nextInt();
+		String cambioEscolhido = Cambio.pesquisarOpcao(opcaoCambio);
 		
-		/**
-		 * Percorre todos os valores de Cambio e recebe seus valores em "cambio" na ordem
-		 * Assim, verifica se o valor da opcao inputada confere com cada opcao de "cambio"
-		 */
-		for (Cambio cambio : Cambio.values()) {
-			if(opcaoCambio == cambio.getNumOpcao())
-				cambioEscolhido = cambio.getNomeCambio();	
-		} 
+		if (cambioEscolhido.equals(null))
+			throw new IllegalArgumentException("Cambio: Opção Inválida.");
 		
-		if (!cambioEscolhido.equals(null))
-			setCambio(cambioEscolhido);
-		else 
-			throw new IllegalArgumentException("Opcao nao valida!");
+		setCambio(cambioEscolhido);
 	}
 	
 	
