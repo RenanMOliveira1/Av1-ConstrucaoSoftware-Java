8345235143abb92938f3b2dbef879609aeb4f759
tiagohs <tiago.silva.93@hotmail.com>
2015-08-13 14:43:16 -0300

    Classes de Excessoes Personalizadas Criadas, só faltando a Imprementação

---
+++ src/com/concessionaria/administracao/excessoes/ChassiDuplicadoException.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2015, GEC5 and/or its affiliates. All rights reserved.
+ * INSTITUTO INFNET. Use is subject to license terms.
+ * 
+ * 
+ * 
+ */
+package com.concessionaria.administracao.excessoes;
+
+/**
+ * @author Tiago Henrique
+ * @since JDK 1.8
+ *
+ */
+public class ChassiDuplicadoException extends Exception {
+
+}
